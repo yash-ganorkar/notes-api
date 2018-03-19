6aec6c433651ea67b696e0d696536cec30d7317f
yash ganorkar <yash.ganorkar@outlook.com>
2018-03-19 11:55:49 -0500

    Commit Message #4: Added GET /todos and GET /todos/:id routes

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 const {User} = require('./models/User');
@@ -33,6 +34,43 @@ app.post('/todo', (request, response) => {
         text: request.body.text
     });
     user.save().then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response.status(400).send({
+            errorMessage: 'error occured ' + error
+        });
+    });
+});
+
+//GET /todos
+app.get('/todos', (request, response) => {
+
+
+    Todo.find({userid: request.headers['bearer']}).then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response.status(400).send({
+            errorMessage: 'error occured ' + error
+        });
+    });
+});
+
+//GET /todos:id
+app.get('/todos/:id', (request, response) => {
+
+    if (request.headers['bearer'] === '') {
+        response.status(400).send({
+            errorMessage: "Bearer token required"
+        })
+    }
+
+    else if (!ObjectID.isValid(request.params.id)) {
+        response.status(404).send({
+            errorMessage: "Invalid id."
+        })
+    }
+
+    Todo.find({_id: request.params.id}).then((doc) => {
         response.send(doc);
     }, (error) => {
         response.status(400).send({
